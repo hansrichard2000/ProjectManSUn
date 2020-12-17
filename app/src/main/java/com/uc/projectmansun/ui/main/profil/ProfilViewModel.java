15fe95bb0ba3f33c19440c7f21a34529d1c54914
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-12-17 14:15:26 +0700

    Logout succeed

---
+++ app/src/main/java/com/uc/projectmansun/ui/main/profil/ProfilViewModel.java
@@ -0,0 +1,35 @@
+package com.uc.projectmansun.ui.main.profil;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.uc.projectmansun.repository.ProfilRepository;
+
+public class ProfilViewModel extends ViewModel {
+    private ProfilRepository repository;
+    private static final String TAG = "ProfilViewModel";
+
+    public ProfilViewModel(){
+
+    }
+
+    public void init(String token) {
+        Log.d(TAG, "init: " + token);
+        repository = ProfilRepository.getInstance(token);
+    }
+
+    public LiveData<String> logout() {
+        return repository.logout();
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        Log.d(TAG, "onCleared: ");
+        repository.resetInstance();
+    }
+
+
+}
