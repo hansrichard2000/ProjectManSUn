42fe777a785c9e9e7063260035a346855938ab43
ReinerJasin <reinerjasin@gmail.com>
2021-01-02 22:51:05 +0800

    isi constant

--- app/src/main/java/com/uc/projectmansun/util/Constants.java
+++ app/src/main/java/com/uc/projectmansun/util/Constants.java
@@ -2,4 +2,5 @@ package com.uc.projectmansun.util;
 
 public class Constants {
     public final static String BASE_URL = "http://192.168.1.72/mansun/public/api/";
+//    public final static String BASE_URL = "http://192.168.1.11/mansun/public/api/";
 }
